e1ba8748fab203fe62b7dac2b3cd05088d1dafe4
Haz Æ 41 <hazae41@gmail.com>
2020-10-25 21:22:53 +0100

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,1 +1,26 @@
-// Hack me!
+import type { Player } from "saurus/player.ts"
+import type { Server } from "saurus/server.ts"
+
+export class MyServerPlugin {
+  /**
+   * Enable MyPlugin on the given server
+   * @param server Server to enable the plugin on
+   */
+  constructor(
+    readonly server: Server
+  ) {
+    console.log("MyPlugin enabled on server", server.name)
+  }
+}
+
+export class MyPlayerPlugin {
+  /**
+   * Enable MyPlugin on the given player
+   * @param player Player to enable the plugin on
+   */
+  constructor(
+    readonly player: Player
+  ) {
+    console.log("MyPlugin enabled on player", player.name)
+  }
+}
